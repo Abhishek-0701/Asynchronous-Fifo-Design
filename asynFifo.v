2e2941b8b82ab0453232d5c3dcb6044906d7c861
Abhishek-0701 <129402170+Abhishek-0701@users.noreply.github.com>
2025-08-20 23:01:51 +0530

    Update asynFifo.v

--- asynFifo.v
+++ asynFifo.v
@@ -29,7 +29,7 @@ wire[adress_size-1:0] rd_pntr_g;
  
  // decalring 2darray
 
-   reg [data width-1:0] mem [fifo_deoth-1:0];
+   reg [data width-1:0] mem [fifo_depth-1:0];
 
 // writing data into FIFO
 
